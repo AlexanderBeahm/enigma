0ecf6e552623689c7a81b9fa3837212e629cd723
Alex Beahm <alexanderbeahm@gmail.com>
2020-10-02 18:20:03 -0500

    Basic decryption via CLI args

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ import base64
 import cli_processor
 
 def main(args):
-    encoded_string = args[1]
-    iterations = int(args[2])
-    print(encoded_string)
-    print(iterations)
-    result_string = encoded_string
-    for x in range(iterations):
+    result_string = args.string
+    for x in range(args.iterations):
          result_string = base64.b64decode(result_string)
     print(result_string.decode('utf-8'))
 
 args = sys.argv
-main(args)
+instructions = cli_processor.process_cli_args(args)
+main(instructions)
