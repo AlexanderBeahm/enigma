fbc79963d46ada46c39940b5ea68fe1e12444caf
Alex Beahm <alexanderbeahm@gmail.com>
2020-10-02 21:38:45 -0500

    Decoding w/ intertext removals and encoding working

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import sys
 import base64
 import cli_processor
+import re
 
 def main(args):
     result_string = args.string
     for x in range(args.iterations):
-         result_string = base64.b64decode(result_string)
-    print(result_string.decode('utf-8'))
+        if(args.mode == 'decode'):
+            result_string = re.sub('\(.*\)', '', base64.b64decode(result_string).decode('utf-8'))
+        else:
+            result_string = base64.b64encode(result_string.encode('utf-8')).decode('utf-8')
+    print(result_string)
 
 args = sys.argv
 instructions = cli_processor.process_cli_args(args)
